ab3ad93d3ab1ecf641c49d375318997a8b8edaf1
Phillip Ridlen <p@rdln.net>
2017-12-05 21:38:08 -0800

    Day 2 part 2

--- 2017/12/02/spreadsheet.rb
+++ 2017/12/02/spreadsheet.rb
@@ -3,10 +3,27 @@ class Spreadsheet
     @input = input.chomp
   end
 
-  def checksum
+  def difference_checksum
+    reduce_rows do |row|
+      row.max - row.min
+    end
+  end
+
+  def divisor_checksum
+    reduce_rows do |row|
+      row.combination(2).each do |pair|
+        pair = pair.sort.reverse
+        break pair.reduce(:/) if pair.reduce(:%) == 0
+      end
+    end
+  end
+
+  private
+
+  def reduce_rows
     @input.each_line.reduce(0) do |sum, row|
       row = row.split(/(\t|\s)/).select {|n| n =~ /\d/ }.map(&:to_i)
-      sum += row.max - row.min
+      sum += yield row
     end
   end
 end
--- 2017/12/02/spreadsheet_test.rb
+++ 2017/12/02/spreadsheet_test.rb
@@ -3,8 +3,8 @@ require_relative "spreadsheet"
 
 class SpreadsheetTest < Minitest::Test
 
-  def test_checksum_with_sample_input
-    sample_input = <<-SAMPLE
+  def test_difference_checksum_with_sample_input
+    sample_input = <<~SAMPLE
       5 1 9 5
       7 5 3
       2 4 6 8
@@ -12,13 +12,32 @@ class SpreadsheetTest < Minitest::Test
 
     spreadsheet = Spreadsheet.new(sample_input)
 
-    assert_equal 18, spreadsheet.checksum
+    assert_equal 18, spreadsheet.difference_checksum
   end
 
-  def test_checksum_with_challenge_input
+  def test_difference_checksum_with_challenge_input
     challenge_input = File.read(File.join(File.dirname(__FILE__), "spreadsheet_input.txt"))
     spreadsheet = Spreadsheet.new(challenge_input)
 
-    assert_equal 43074, spreadsheet.checksum
+    assert_equal 43074, spreadsheet.difference_checksum
+  end
+
+  def test_divisor_checksum_with_sample_input
+    sample_input = <<~SAMPLE
+      5 9 2 8
+      9 4 7 3
+      3 8 6 5
+    SAMPLE
+
+    spreadsheet = Spreadsheet.new(sample_input)
+
+    assert_equal 9, spreadsheet.divisor_checksum
+  end
+
+  def test_divisor_checksum_with_challenge_input
+    challenge_input = File.read(File.join(File.dirname(__FILE__), "spreadsheet_input.txt"))
+    spreadsheet = Spreadsheet.new(challenge_input)
+
+    assert_equal 280, spreadsheet.divisor_checksum
   end
 end
