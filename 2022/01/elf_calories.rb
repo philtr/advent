46800abab2f81172441e0654bcb2332ce67482ba
Phillip Ridlen <p@rdln.net>
2022-12-01 12:25:54 -0600

    Use endless method definition

--- 2022/01/elf_calories.rb
+++ 2022/01/elf_calories.rb
@@ -1,31 +1,21 @@
 module ElfCalories
-  ELF_SEP = "\n\n"
-  ITEM_SEP = "\n"
+  ELF_SEP, ITEM_SEP = ["\n\n", "\n"]
 
   class << self
 
-    def most_calories(input)
-      process_input(input).max
-    end
-
-    def top(input, n)
-      process_input(input).max(3).sum
-    end
+    def most_calories(input) = process_input(input).max
+    def top(input, n) = process_input(input).max(3).sum
 
     private
 
-    def process_input(input)
-      input
-        .split(ELF_SEP)
-        .map(&method(:process_elf))
-    end
+    def process_input(input) = input
+      .split(ELF_SEP)
+      .map(&method(:process_elf))
 
-    def process_elf(elf_input)
-      elf_input
-        .split(ITEM_SEP)
-        .map(&:to_i)
-        .sum
-    end
+    def process_elf(elf_input) = elf_input
+      .split(ITEM_SEP)
+      .map(&:to_i)
+      .sum
 
   end
 end
