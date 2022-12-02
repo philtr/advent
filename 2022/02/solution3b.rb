c9c3d96951b193ab82a72cac8e10aacd66ca63a5
Phillip Ridlen <p@rdln.net>
2022-12-02 16:23:28 -0600

    Another round of golf

--- 2022/02/solution3b.rb
+++ 2022/02/solution3b.rb
@@ -1,1 +1,1 @@
-A=1;B=2;C=3;X=[0,-1];Y=[3,0];Z=[6,1];p $<.reduce(0){|s,m|m=m.split(" ").map{eval(_1)};s+m[1][0]+(a=(1..3).cycle(3).to_a)[a.index(m[0])+3+m[1][1]]}
+A,B,C=a=[1,2,3];X,Y,Z=[0,-1],[3,0],[6,1];p $<.sum{|m|m=m.split.map{eval(_1)};m[1][0]+a[(a.index(m[0])+m[1][1])%3]}
