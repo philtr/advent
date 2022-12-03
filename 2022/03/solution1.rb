46c7a487327514ec7a712c8d17a02fe3f7f7726d
Phillip Ridlen <p@rdln.net>
2022-12-03 09:29:37 -0600

    Day 3 third round of golf

--- 2022/03/solution1.rb
+++ 2022/03/solution1.rb
@@ -1,3 +1,3 @@
-I=$<.readlines.map{|l|l.chomp.bytes.map{_1>90?_1-96:_1-38}}
-p I.sum{|r|r.each_slice(r.size/2).reduce{_1&_2}[0]}
-p I.each_slice(3).sum{|g|g.reduce{_1&_2}[0]}
+I=$<.readlines.map{|l|l.chomp.bytes.map{_1-(_1>90?96:38)}}
+p I.sum{_1.each_slice(_1.size/2).reduce(&:&)[0]}
+p I.each_slice(3).sum{_1.reduce(&:&)[0]}
