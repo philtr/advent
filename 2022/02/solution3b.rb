1e799432670b422c3244b0b1842d55bcac74134b
Phillip Ridlen <p@rdln.net>
2022-12-05 10:29:33 -0600

    2022 Day 02 last golf

--- 2022/02/solution3b.rb
+++ 2022/02/solution3b.rb
@@ -1,1 +1,1 @@
-A,B,C=a=[1,2,3];X,Y,Z=[0,-1],[3,0],[6,1];p $<.sum{|m|m=m.split.map{eval(_1)};m[1][0]+a[(a.index(m[0])+m[1][1])%3]}
+A,B,C=M=[1,2,3];X,Y,Z=[0,-1],[3,0],[6,1];p $<.sum{|m|t,(s,m)=m.split.map{eval(_1)};s+M[(M.index(t)+m)%3]}
