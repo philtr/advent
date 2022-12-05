74f8f1de234bba9628e2bc398b7ae21159237b65
Phillip Ridlen <p@rdln.net>
2022-12-05 08:48:28 -0600

    2022 Day 5 Part 2

--- 2022/05/solution.rb
+++ 2022/05/solution.rb
@@ -3,30 +3,29 @@ def init(stacks, line)
     stacks[stack] ||= []
     stacks[stack].unshift(crate) if crate =~ /[A-Z]/
   end
-  p stacks
+  stacks
 end
 
-def move(stacks, line)
+def move(stacks, line, part2 = false)
   count, from, to = line.scan(/\d+/).map { _1.to_i-1 }
-  p "move #{count+1} from #{from+1} to #{to+1}"
   stacks[to] ||= []
   stacks[from] ||= []
-  stacks[to].push(*(stacks[from].pop(count+1).reverse))
-  p stacks
+  crates = stacks[from].pop(count+1)
+  part2 ? :noop : crates.reverse!
+  stacks[to].push(*crates)
+  stacks
 end
 
 File.open ARGV[0] do |f|
   stacks = []
-  n = 0
   until f.eof?
     stacks =
       case (line = f.readline)
       when /\A\s*\[/ then init(stacks, line)
-      when /\Amove/ then move(stacks, line)
+      when /\Amove/ then move(stacks, line, ARGV[1].to_i == 2)
       else stacks
       end
   end
 
   puts stacks.map(&:pop).join
 end
-
