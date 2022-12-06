1a7d6bce1c6575184e23e2d964612d551b9c57bd
Phillip Ridlen <p@rdln.net>
2022-12-05 23:36:25 -0600

    2022 Day 06 Part 2

--- 2022/06/solution.rb
+++ 2022/06/solution.rb
@@ -1,21 +1,37 @@
-require "irb"
 require "minitest/autorun"
 require "minitest/spec"
 
-def packet_start(signal)
-  signal.chars.each_cons(4).each_with_index do |chars, index|
-    return index+4 if chars.uniq.size == 4
+module Communicator
+
+  module_function
+
+  def packet_start(signal) = first_unique_chars(signal, 4)
+  def message_start(signal) = first_unique_chars(signal, 14)
+
+  def first_unique_chars(signal, n)
+    signal.chars.each_cons(n).each_with_index do |chars, index|
+      return index+n if chars.uniq.size == n
+    end
   end
 end
 
-describe "find start of packet" do
+puts "First start-of-packet marker: #{Communicator.packet_start(File.read("input.txt"))}"
+puts "First start-of-message marker: #{Communicator.message_start(File.read("input.txt"))}"
+
+describe Communicator do
   it "finds the marker correctly" do
-    value(packet_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).must_equal(7)
-    value(packet_start("bvwbjplbgvbhsrlpgdmjqwftvncz")).must_equal(5)
-    value(packet_start("nppdvjthqldpwncqszvftbrmjlhg")).must_equal(6)
-    value(packet_start("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).must_equal(10)
-    value(packet_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).must_equal(11)
+    value(Communicator.packet_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).must_equal(7)
+    value(Communicator.packet_start("bvwbjplbgvbhsrlpgdmjqwftvncz")).must_equal(5)
+    value(Communicator.packet_start("nppdvjthqldpwncqszvftbrmjlhg")).must_equal(6)
+    value(Communicator.packet_start("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).must_equal(10)
+    value(Communicator.packet_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).must_equal(11)
+  end
+
+  it "finds the marker correctly" do
+    value(Communicator.message_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).must_equal(19)
+    value(Communicator.message_start("bvwbjplbgvbhsrlpgdmjqwftvncz")).must_equal(23)
+    value(Communicator.message_start("nppdvjthqldpwncqszvftbrmjlhg")).must_equal(23)
+    value(Communicator.message_start("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).must_equal(29)
+    value(Communicator.message_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).must_equal(26)
   end
 end
-
-puts "First marker: #{packet_start(File.read("input.txt"))}"
