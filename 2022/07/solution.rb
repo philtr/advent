fd14a5eb7505b68286a42e53dbe905fb3245a367
Phillip Ridlen <p@rdln.net>
2022-12-07 01:57:17 -0600

    Clean up

--- 2022/07/solution.rb
+++ 2022/07/solution.rb
@@ -1,65 +1,63 @@
-require "minitest/autorun"
-require "minitest/spec"
+class CommunicatorStorage
 
-class Communicator
+  attr_reader :total_space
+  attr_accessor :usage
 
-  attr_reader :io, :disk_usage, :total_disk_space
-
-  def initialize(io, total_disk_space: 70000000)
-    @io = io
-    @disk_usage = calculate_disk_usage
-    @total_disk_space = total_disk_space
+  def initialize(total_space: 70000000)
+    @usage = Hash.new(0)
+    @total_space = total_space
   end
 
-  def dir_sum_max(max = 100_000)
-    disk_usage.sum { _2 <= max ? _2 : 0 }
-  end
-
-  def space_used = disk_usage["/"]
-  def space_free = total_disk_space - space_used
-
-  def make_free_space(min = 30_000_000)
-    bytes_to_delete = min - space_free
-
-    disk_usage
-      .select {|_dir, size| size > bytes_to_delete }
-      .sort_by(&:last)
-      .first
-  end
-
-  private
-
-  def calculate_disk_usage
-    du = Hash.new(0)
+  def calculate_usage(io)
     cwd = []
 
     until io.eof? do
-      case (cmd = io.gets.chomp)
-      when "$ cd /" then :noop
-      when "$ cd .." then cwd.pop
-      when /\$ cd (.+)/ then cwd.push([*cwd, $1].compact.join("/"))
+      case io.gets.chomp
+
+      when "$ cd .."
+        cwd.pop
+
+      when /\$ cd ([^\/]+)/
+        cwd.push([*cwd, $1].compact.join("/"))
+
       when /(\d+)/ then
         size = $1.to_i
-        du["/"] += $1.to_i
-        cwd.each { du[_1] += $1.to_i }
+        usage["/"] += $1.to_i
+        cwd.each { usage[_1] += $1.to_i }
       end
     end
-
-    du
   end
 
+  def space_used = usage["/"]
+
+  def space_free = total_space - space_used
+
+  def dir_sum_max(max) = usage.sum {|_, size| size <= max ? size : 0 }
+
+  def make_free_space(min)
+    usage
+      .select {|_, size| size > (min - space_free) }
+      .sort_by(&:last)
+      .first
+  end
 end
 
-comm = Communicator.new(File.open("input.txt"))
+comm = CommunicatorStorage.new
+comm.calculate_usage(File.open("input.txt"))
 
 puts "=========================================================="
 puts "Part 1: #{comm.dir_sum_max(100_000)}"
 puts "Part 2: #{comm.make_free_space(30_000_000)[1]}"
 puts "=========================================================="
 
-describe Communicator do
+require "minitest/autorun"
+require "minitest/spec"
+
+describe CommunicatorStorage do
   let(:io) { File.open("example.txt") }
-  let(:subject) { Communicator.new(io) }
+  let(:subject) { CommunicatorStorage.new }
+
+  before { subject.calculate_usage(io) }
 
   describe "Part 1" do
     it "finds the sum of all directories of size <= 100000" do
