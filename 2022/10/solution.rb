65f1465e34597f8888a527c2725e40f46aaf6026
Phillip Ridlen <p@rdln.net>
2022-12-10 00:43:20 -0600

    2022 Day 10 Part 2

--- 2022/10/solution.rb
+++ 2022/10/solution.rb
@@ -1,14 +1,25 @@
-cycles = $<.reduce([{1=>1}, 1]) do |(cycles, i), ins|
+cycles, _i, screen = $<.reduce([{0=>1}, 0, ""]) do |(cycles, i, screen), ins|
+  cursor = cycles[i] % 40
+  sprite = (cursor-1)..(cursor+1)
+  screen << (sprite.cover?(i % 40) ? "#" : ".")
+
+  # process instruction
   case ins
-  when /n/ then next [cycles.merge(i+1 => cycles[i]), i+1]
+  when /n/ then next [ cycles.merge(i+1 => cycles[i]),
+                       i+1,
+                       screen ]
   when /-?\d+/ then
     cycles[i+1] = cycles[i]
     cycles[i+2] = cycles[i] + $&.to_i
-    next [cycles, i+2]
-  end
-  raise "should not get here"
-end[0]
 
+    screen << (sprite.cover?((i+1) % 40) ? "#" : ".")
+
+    next [cycles, i+2, screen]
+  end
+end
+
+# part 1
 puts 20.step(by: 40, to: cycles.size).map { cycles[_1] * _1 }.reduce(&:+)
 
-
+# part 2
+puts screen.chars.each_slice(40).map(&:join)
