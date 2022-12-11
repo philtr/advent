9b7d050dc48253cf792074a4a7842519a89bf4e9
Phillip Ridlen <p@rdln.net>
2022-12-10 22:42:02 -0600

    I don't love real golf but code golf is irresistible

--- 2022/10/golf.rb
+++ 2022/10/golf.rb
@@ -1,7 +1,8 @@
-X,O,F=[40,?▓,?░]
-C,S,_=(1..240).reduce([[0,1],[],[0]]){|(c,s,a),i|m=(c[i]+1)%X
-s<<(((m-1)..(m+1)).cover?(i%X)?O : F)
-a.any?||a=[0,$<.gets[/-?\d+/].to_i].uniq
+X=40;R=(1..240).reduce([[1,1],"",[0],0]){|(c,s,a,k),i|m=(c[i]+1)%X
+s<<(((m-1)..(m+1)).cover?(i%X)??▓:?░)
+i%X>0||s<<"\n"
+a!=[]||a=[0,$<.gets[/-?\d+/].to_i]|[]
 c[i+1]=c[i]+a.pop
-[c,s,a]}
-puts [((20..220).step(X)).map{C[_1]*_1}.reduce(&:+),S.each_slice(X).map(&:join)]
+(i+20)%X<1&&k+=c[i]*i
+[c,s,a,k]}
+puts(R[3],R[1])
